54601765ee7206dfc6448076e7a8ef98aba08ce0
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-05-30 13:09:49 +0900

    3차 미완성 소스코드

---
+++ sketch_nov30c.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
+
+int relay = 2;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop(){
+  
+  float soil = analogRead(A1);
+  Serial.println(soil);
+
+  if(soil < 800 ){
+    digitalWrite(relay, LOW);
+    delay(1000);
+    lcd.setCursor(0,0);
+    lcd.print("moisture : ");
+    lcd.print(soil);
+    delay(1000);
+    
+  }
+  else{
+    digitalWrite(relay, HIGH);
+    delay(2000);
+    digitalWrite(relay, LOW);
+    delay(2000);
+    lcd.setCursor(0,0);
+    lcd.print("moisture : ");
+    lcd.print(soil);
+    delay(1000);
+  }
+  delay(5000);
+
+}
